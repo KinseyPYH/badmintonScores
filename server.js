f125e69209413fbb9e928440a237a2f44315135b
KinseyPYH <kinsey@fredho.net>
2021-03-17 09:50:40 -0700

    Push for server

--- server.js
+++ server.js
@@ -170,7 +170,8 @@ app.post("/scores", (req, res, next) => {
 
 
 // GET API INFO EVERY THIRTY MIN
-var thirtyMin = 30*60*1000;
+var thirtyMin //= 30*60*1000;
+thirtyMin = 60000
 setInterval(function() {
   getDateTime(),
   getScores()}, thirtyMin);
