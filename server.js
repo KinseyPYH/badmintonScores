a911d66af1fcac3ccfa6432d84c9f619b59babeb
KinseyPYH <kinsey@fredho.net>
2021-03-17 07:47:23 -0700

    SSL 2nd try

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.use(express.json());
 
 
 
-app.listen(80, () => {
- console.log("Server running on port 80");
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
  console.log(getDateTime());
 });
 
